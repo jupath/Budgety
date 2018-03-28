4ad4600afaf8a58936626e2df51b692453514ab2
Jupath <attila.taligas@gmail.com>
2018-03-28 21:32:12 +0200

    Update Page Not Found

--- src/components/pages/PageNotFound.js
+++ src/components/pages/PageNotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PageNotFound = () => (
   <div className="text-center pt-5">
-    <h2>404 error!</h2>
+    <h2>Ooops, error 404!</h2>
     <p>Page not found!</p>
   </div>
 );
